a3f4041968f5ded37cdd2e462c7bd70eab083201
onedaymore <andayfour@gmail.com>
2017-05-22 23:24:52 +0800

    spring boot and rabbitMQ

---
+++ rabbitmq-hello/src/main/java/hello/RabbitController.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RabbitController {
+	
+	@Autowired
+	private Sender sender;
+
+	@RequestMapping(value = "/send", method = RequestMethod.GET)
+	public String send() {
+		sender.send();
+		return "send";
+	}
+}
